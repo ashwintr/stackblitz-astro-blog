718d7501a11e2966bcee465fe12f772f5f0ec02f
ashwintr <ashwintr10@gmail.com>
2024-12-24 17:10:59 +0530

    Added Astro-umani and its dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,26 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
-import remarkGfm from 'remark-gfm'
-import remarkSmartypants from 'remark-smartypants'
-import rehypeExternalLinks from 'rehype-external-links'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
+
+import yeskunallumami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ashwintr.github.io',
   base: 'stackblitz-astro-blog',
-  integrations: [mdx(), svelte()],
+  integrations: [mdx(), svelte(), yeskunallumami({
+    id: "b08debc9-2b81-4109-92e4-4160d8bee80e"
+  })],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: 'nord'
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank'
+    }]]
+  }
+});
